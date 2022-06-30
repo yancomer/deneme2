90f421ef89b02fb5e313ba80e345441471c1a34d
yancomer <omer.ync19@gmail.com>
2022-06-30 15:45:10 +0300

    quiz formu oluşturuldu jquery kurulumu yapıldı

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @livewireStyles
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
@@ -38,14 +38,15 @@
             
             <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            {{ $slot }}</div>
+            {{ $slot }}
+        </div>
     </div>
                 
             
         </div>
 
         @stack('modals')
-
+        {{$js}}
         @livewireScripts
     </body>
 </html>

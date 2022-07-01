5a298d5f70ee1b1bdea2dcfc4eb911af6bf4c543
yancomer <omer.ync19@gmail.com>
2022-07-01 10:06:36 +0300

    hata mesajı vermesi uygulandı quiz bilgisi güncellemesi uygulandı

---
+++ app/Http/Requests/QuizUpdateRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class QuizUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function rules()
+    {
+        return [
+            'title'=>'required|min:3|max:200',
+            'description'=>'max:1000',
+            'finished_at'=>'nullable|after:'.now()
+        ];
+    }
+    public function attributes(){
+        return [
+            'title'=>'Quiz Başlığı',
+            'description'=>'Quiz Açıklama',
+            'finished_at'=>'Bitiş Tarihi'
+        ];
+    }
+}

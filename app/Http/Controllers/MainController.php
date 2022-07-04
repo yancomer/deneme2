5a185a8bd0f76eaf899719a4f78c8b9caa0177a6
yancomer <omer.ync19@gmail.com>
2022-07-04 11:42:40 +0300

    Soru tablosu oluşturuldu

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,4 +20,8 @@ class MainController extends Controller
         $quiz = Quiz::whereSlug($slug)->withCount('questions')->first() ?? abort(404,'quiz bulunamadı');
         return view('quiz_detail',compact('quiz'));
     }
+
+    public function result(Request $request, $slug){
+            return $request->post();
+    }
 }
--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -2,8 +2,9 @@
     <x-slot name="header">{{$quiz->title}}</x-slot>
     <div class="card">
   <div class="card-body">
-    <form method="POST" action="#">
-            @foreach($quiz->questions as $question)
+    <form method="POST" action="{{route('quiz.result',$quiz->slug)}}">
+            @csrf
+    @foreach($quiz->questions as $question)
 
             <strong>{{$loop->iteration}}. Soru </strong> {{$question->question}}
             @if($question->image)

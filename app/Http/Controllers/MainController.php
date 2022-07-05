2fb87927f35e28a1e33f37d86c94a4a21a75ef7c
yancomer <omer.ync19@gmail.com>
2022-07-05 10:47:05 +0300

    Result tablosu oluşturuldu. Sonuç hesaplaması yapıldı.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Quiz;
+use App\Models\Answer;
+use App\Models\Result;
 
 class MainController extends Controller
 {
@@ -22,6 +24,31 @@ class MainController extends Controller
     }
 
     public function result(Request $request, $slug){
-            return $request->post();
+            $quiz = Quiz::with('questions')->whereSlug($slug)->first() ?? abort(404,'quiz bulunamadı');
+            $correct = 0;
+            foreach($quiz->questions as $question){
+                Answer::create([
+                    'user_id'=>auth()->user()->id,
+                    'question_id'=>$question->id,
+                    'answer'=>$request->post($question->id)
+                ]);
+
+                if($question->correct_answer===$request->post($question->id)){
+                    $correct+=1;
+                }
+            }
+            
+            $point = round((100/count($quiz->questions)) * $correct);
+            $wrong = count($quiz->questions)-$correct;
+
+            Result::create([
+                'user_id'=>auth()->user()->id,
+                'quiz_id'=>$quiz->id,
+                'point'=>$point,
+                'correct'=>$correct,
+                'wrong'=>$wrong,
+            ]);
+
+            return redirect()->route('quiz.detail',$quiz->slug)->withSuccess("Başarıyla quizi bitirdin. Puanın: ".$point);
     }
 }

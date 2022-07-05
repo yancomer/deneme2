885dde086761e9ab0152b4b8babbb2a6494af0c2
yancomer <omer.ync19@gmail.com>
2022-07-05 12:20:39 +0300

    Quiz verisi çıkarma işlemleri getirildi Kullanıcı hata düzenlemesi yapıldı

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -19,13 +19,17 @@ class MainController extends Controller
     }
 
     public function quiz_detail($slug){
-        $quiz = Quiz::whereSlug($slug)->withCount('questions')->first() ?? abort(404,'quiz bulunamadı');
+        $quiz = Quiz::whereSlug($slug)->with('my_result','results')->withCount('questions')->first() ?? abort(404,'quiz bulunamadı');
         return view('quiz_detail',compact('quiz'));
     }
 
     public function result(Request $request, $slug){
             $quiz = Quiz::with('questions')->whereSlug($slug)->first() ?? abort(404,'quiz bulunamadı');
             $correct = 0;
+
+            if($quiz->my_result){
+                abort(404,'Bu quize daha önce katıldınız');
+            }
             foreach($quiz->questions as $question){
                 Answer::create([
                     'user_id'=>auth()->user()->id,
--- resources/views/quiz_detail.blade.php
+++ resources/views/quiz_detail.blade.php
@@ -6,6 +6,19 @@
     <div class="row">
         <div class="col-md-4">
         <ul class="list-group">
+        @if($quiz->my_result)
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+     Puan
+    <span title="{{$quiz->finished_at}}" class="badge badge-primary badge-pill">{{$quiz->my_result->point}}</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+     Doğru / Yanlış Sayısı
+     <div class="float-right">
+    <span title="{{$quiz->finished_at}}" class="badge badge-success badge-pill">{{$quiz->my_result->correct}}Doğru</span>
+    <span title="{{$quiz->finished_at}}" class="badge badge-danger badge-pill">{{$quiz->my_result->wrong}}Yanlış</span>
+</div>
+  </li>
+  @endif
             @if($quiz->finished_at)
         <li class="list-group-item d-flex justify-content-between align-items-center">
     Son Katılım Tarihi
@@ -16,21 +29,27 @@
     Soru Sayısı
     <span class="badge badge-secondary badge-pill">{{$quiz->questions_count}}</span>
   </li>
+  @if($quiz->details)
   <li class="list-group-item d-flex justify-content-between align-items-center">
     Katılımcı Sayısı
-    <span class="badge badge-secondary badge-pill">10</span>
+    <span class="badge badge-warning badge-pill">{{ $quiz->details['join_count']}}</span>
   </li>
   <li class="list-group-item d-flex justify-content-between align-items-center">
     Ortalama Puan
-    <span class="badge badge-primary badge-pill">60</span>
+    <span class="badge badge-light badge-pill">{{$quiz->details['average']}}</span>
   </li>
+  @endif
 </ul>
         </div>
         <div class="col-md-8">
         {{$quiz->description}}
     </p>
+    @if($quiz->my_result)
+    <a href="{{route('quiz.join', $quiz->slug)}}" class="btn btn-primary btn-block">Quiz'i görüntüle</a>
+    @else
     <a href="{{route('quiz.join', $quiz->slug)}}" class="btn btn-primary btn-block">Quiz'e katıl</a>
-        </div>
+    @endif      
+  </div>
     </div>    
   </div>
 </div>

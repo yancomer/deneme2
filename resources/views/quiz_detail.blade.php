422bed444dac1dc6ea29932be8f3ca6af6f96e89
yancomer <omer.ync19@gmail.com>
2022-07-06 11:32:37 +0300

    Sıralama listesi getirildi Profil düzenleme hatası görüldü

--- resources/views/quiz_detail.blade.php
+++ resources/views/quiz_detail.blade.php
@@ -40,6 +40,24 @@
   </li>
   @endif
 </ul>
+@if(count($quiz->topTen)>0)
+<div class="card mt-3">
+  <div class="card-body">
+    <h5 class="card-title">İlk 10</h5>
+    <ul class="list-group">
+    @foreach($quiz->topTen as $result)  
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <strong class="h6">{{$loop->iteration}}.</strong>
+      <img class="w-8 h-8 rounded-full" src="{{$result->user->profile_photo_url}}">
+
+      {{$result->user->name}}
+      <span class="badge badge-success badge-pill">{{$result->point}}</span>
+    </li>
+    @endforeach
+</ul>
+  </div>
+</div>
+@endif
         </div>
         <div class="col-md-8">
         {{$quiz->description}}
